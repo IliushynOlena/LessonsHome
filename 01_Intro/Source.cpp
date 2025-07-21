8dc487533ca8cec918e97dbf52ec73922daf9c91
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2025-07-21 17:46:43 +0300

    Add project files.

---
+++ 01_Intro/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	//     \n
+	//     \t
+	//     \"
+	//     \'
+	cout << "...:::RESUME:::... \n \n Name: \t Ivan \n Surname: Ivanov \n Last name: Ivanovich \n \n Date of birth: \t 22.06.06 \n City: Rivne \n Age: 17 \n \n Hobby: \t C++ \n ::..................................................:: \n";
+
+
+
+
+
+
+}
---
+++ 02_DataTypes/02_DataTypes.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+   //numbers
+	//symbols
+	//logic
+
+	/*double a = 3.36;
+	cout << a << endl;
+	cout << " a " << a << endl;
+	float b = 1.45;
+	cout << " b " << b << endl;*/
+
+	// INT a = 5;// error
+	int age = 15;
+	cout << " age " << age << endl;
+	int Age = 16;
+	cout << " Age " << Age << endl;
+
+	int ageofman = 40;
+	cout << " ageofman " << ageofman << endl;
+	int ageOfMan = 55;
+	cout << " ageOfMan " << ageOfMan << endl;
+	int age_of_man = 100;
+	cout << " age_of_man " << age_of_man << endl;
+
+	const float PI = 3.14;
+	cout << " PI " << PI << endl;
+	//PI = 100;
+
+	cout << " PI " << PI << endl;
+	int a1 = 5;
+	int a2 = 6;
+	int a3 = 7;
+
+	//int 4a = 7;//error
+	//int int = 77;
+
+	int DaysIn2000Years = 366;
+	const int HourInDay = 24;
+	int HourIn2000Year;
+
+	HourIn2000Year = DaysIn2000Years * HourInDay;
+	cout << "HourIn2000Year = " << HourIn2000Year << endl;
+
+
+	float discount = 0.5;
+	float cost = 21.50;
+	cout << "Enter cost : ";
+	cin >> cost;
+	int count = 10;
+	cout << "Enter count : ";
+	cin >> count;
+	float price;
+	price = cost * count - cost * count *discount;
+	cout << "You have to pay : " << price << endl;
+
+	int number;
+	cout << "Enter number : ";
+	cin >> number;
+	cout << "Number : " << number << endl;
+
+	int a, b, c;
+	cout << "Enter a,b,c";
+	cin >> a >> b >> c;
+	cout << "a = " << a << " b = " << b << " c = " << c << endl;
+
+
+    
+
+
+}
+
+
